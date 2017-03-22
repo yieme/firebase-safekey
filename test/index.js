37454019978657017c87ae5d87edf9e94b2b901f
yieme <yieme@cormea.com>
2017-03-22 09:47:53 -0600

    safe number support

--- test/index.js
+++ test/index.js
@@ -9,10 +9,18 @@ describe('firebase-safekey', function() {
 
   var expected = { 'key<>p1': 'value1', 'key<>o2<>c': 'value2' }
   var expectedString = JSON.stringify(expected)
-  it('should eaual ' + expectedString, function(done) {
+  it('should equal ' + expectedString, function(done) {
     var test = firebaseSafekey.safe({ 'key#1': 'value1', 'key[2]': 'value2' })
     var json = JSON.stringify(test)
     expect(json).to.equal(expectedString);
+    done();
+  });
+
+  var expected2 = '"10<>d1"'
+  it('should equal ' + expected2, function(done) {
+    var test = firebaseSafekey.safe(10.1)
+    var json = JSON.stringify(test)
+    expect(json).to.equal(expected2);
     done();
   });
 
