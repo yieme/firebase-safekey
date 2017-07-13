25bb47b92ee989c1b2c0f3fa9418762de37e02e7
Danny Cochran <daniel@cochrans.org>
2017-07-12 17:41:36 -0700

    default type for string

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@
 declare module 'firebase-safekey' {
   type Mapping = {[from: string]: string};
 
-  export function safe<T>(input: T, mapping?: Mapping): T;
-  export function restore<T>(input: T, mapping?: Mapping): T;
+  export function safe<T = string>(input: T, mapping?: Mapping): T;
+  export function restore<T = string>(input: T, mapping?: Mapping): T;
   export function config(mapping?: Mapping): any;
 }
